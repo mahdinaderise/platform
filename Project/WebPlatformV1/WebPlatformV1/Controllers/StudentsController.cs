9a30380d5bf459a90795f2b4d3cf985d22d85ff1
mohi-amh <m.amirhajlow77@gmail.com>
2020-07-07 13:18:51 +0430

    import students

---
+++ Project/WebPlatformV1/WebPlatformV1/Controllers/StudentsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebPlatformV1.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public IActionResult course()
+        {
+            return View();
+        }
+        public IActionResult ido()
+        {
+            return View();
+        }
+        public IActionResult index()
+        {
+            return View();
+        }
+        public IActionResult studenttask()
+        {
+            return View();
+        }
+        public IActionResult students()
+        {
+            return View();
+        }
+    }
+}
