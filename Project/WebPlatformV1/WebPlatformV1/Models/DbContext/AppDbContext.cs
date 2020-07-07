6bdad2cbaa4010a9c1669b16048611095585274a
mahdinaderise <46739075+mahdinaderise@users.noreply.github.com>
2020-07-08 03:17:01 +0430

    FRIST CODE DB

--- Project/WebPlatformV1/WebPlatformV1/Models/DbContext/AppDbContext.cs
+++ Project/WebPlatformV1/WebPlatformV1/Models/DbContext/AppDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
@@ -13,5 +14,91 @@ namespace WebPlatformV1.Models.DbContext
         {
 
         }
+
+
     }
+    public class Tbl_Blog
+    {   [Key]
+        public int ID { get; set; }
+        public int IDConsultant { get; set; }
+        public int IDAttach { get; set; }
+        public string Note { get; set; }
+    }
+    public class Tbl_Consultant
+    {
+        [Key]
+        public int IDConsultant { get; set; }
+        public string NameConsultant { get; set; }
+        public string FamilyConsultant { get; set; }
+        public int NationlCode { get; set; }
+        public int Tell { get; set; }
+    }
+    public class Tbl_Course
+    {
+        [Key]
+        public int IDCourse { get; set; }
+        public string NameCourse { get; set; }
+    }
+    public class Tbl_Do
+    {
+        [Key]
+        public int IDdo { get; set; }
+        public int IDStudent { get; set; }
+        public int IDTasks { get; set; }
+        public string Note { get; set; }
+        [Required]
+        public int CountTest { get; set; }
+        [Required]
+        public int TimeStudy { get; set; }
+    }
+    public class Tbl_FinnialManegment
+    {
+        [Key]
+        public int IDFinancial { get; set; }
+        public int IDConsultant { get; set; }
+        public int NumReceipt { get; set; }
+        public int DatePayment { get; set; }
+        public string State { get; set; }
+    }
+    public class Tbl_Headline
+    {
+        [Key]
+        public int IDHeadline { get; set; }
+        public int IDCourse { get; set; }
+        [Required]
+        public string HeadlineName { get; set; }
+    }
+    public class Tbl_SendFileConsultant
+    {
+        [Key]
+        public int IDAttach { get; set; }
+        public string link { get; set; }
+        public string type { get; set; }
+    }
+    public class Tbl_Student
+    {
+        [Key]
+        public int IDStudent { get; set; }
+        [Required]
+        public string NameStudent { get; set; }
+        [Required]
+        public string FamilyStudent { get; set; }
+        [Required]
+        public int NationalCode { get; set; }
+        [Required]
+        public int Tell { get; set; }
+        [Required]
+        public string Address { get; set; }
+    }
+    public class Tbl_Tasks
+    {
+        public int IDTasks { get; set; }
+        public int IDCansoltant { get; set; }
+        public int IDTeachers { get; set; }
+        public int IDStudent { get; set; }
+        public int IDido { get; set; }
+        public string NameTasks { get; set; }
+        public DateTime SendDelivery { get; set; }
+    }
+
 }
