1ff153843562a7e9402aeab5879042436ac55e47
MahdiNaderi <46739075+mahdinaderise@users.noreply.github.com>
2020-07-11 23:23:16 +0430

    data

---
+++ Project/WebPlatformV1/WebPlatformV1/Models/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebPlatformV1.Models
+{
+    public class ApplicationUser:IdentityUser
+    {
+        public string Name { get; set; }
+        public string Family { get; set; }
+        public int NationlCode { get; set; }
+        public string State { get; set; }
+    }
+}
